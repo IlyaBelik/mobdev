8b6f68d76b18887a580dc68e6ffc4fd9926dd647
IlyaBelik <37375602+IlyaBelik@users.noreply.github.com>
2019-10-17 10:18:09 +0300

    Update HomeActivity.java

--- app/src/main/java/com/example/mob_lab1/HomeActivity.java
+++ app/src/main/java/com/example/mob_lab1/HomeActivity.java
@@ -26,7 +26,9 @@ public class HomeActivity extends AppCompatActivity {
                 startActivity(intentToMain);
             }
         });
+        
     }
+    
     public void onBackPressed(){
         moveTaskToBack(true);
     }
